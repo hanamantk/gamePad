6393a56a042d8c477e9ba7e7b7d255b22bbf718d
Hanamant Jimmannava <hanamant.kj@SSLTUIODC.LOCAL>
2019-01-24 13:34:11 +0530

    Revert "added"
    This reverts commit c3bbd0ba2dbe82601ed16096b2f4616bef34c46c.

--- src/index.js
+++ src/index.js
@@ -9,4 +9,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 
-var hk =89;
